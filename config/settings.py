de31b65d816cfe397f312db029e099a0c3298dc4
Manas Rathore <manasrathore15@gmail.com>
2026-05-27 20:55:52 +0530

    Secured production environment variables

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.parse(
-        'postgresql://neondb_owner:npg_nJkwd3Layb6h@ep-snowy-heart-apajg301.c-7.us-east-1.aws.neon.tech/neondb?sslmode=require'
+        config('DATABASE_URL')
     )
 }
 
